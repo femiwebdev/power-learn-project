eec7a79711cdf8ee6141f9e0024d5f7249237aff
Solomon Olagoroye <solomon.olagoroye@miva.edu.ng>
2025-09-26 06:57:33 +0100

    Delete learn dbms directory

--- learn dbms/database/ddl/03_indexes.sql
+++
@@ -1,4 +0,0 @@
-CREATE INDEX idx_users_email ON Users(email);
-CREATE INDEX idx_books_title ON Books(title);
-CREATE INDEX idx_loans_user_id ON Loans(user_id);
-CREATE INDEX idx_loans_book_id ON Loans(book_id);
